0b7b88042785cc25e67d6bb04c0a97bad6e88323
valeriadmitrievna1506 <valeriadmitrievna1506@gmail.com>
2020-09-25 09:10:21 +0300

    start tablet adaptive
    but fucking laptop is asshole

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // for all pages
-
-$('.medbook-title').mouseenter(function () {
-  $('.medbook-dropdown').css("display", "flex");
-});
-$('.medbook').mouseleave(function () {
-  $('.medbook-dropdown').css("display", "none");
+$('.medbook-title').click(function () {
+  if ($('.medbook-dropdown').css('display') == 'none') {
+    $('.medbook-dropdown').css('display', 'flex');
+  }
+  else if ($('.medbook-dropdown').css('display') == 'flex') {
+    $('.medbook-dropdown').css('display', 'none');
+  }
 });
 
 $('#menu-burger').click(function() {
