2c5b4f69af1f88193375f7f394a969211d1b4efd
valeriadmitrievna1506 <valeriadmitrievna1506@gmail.com>
2020-09-28 15:28:55 +0300

    edit analysis + main

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// for all pages
-$('.medbook-title').click(function () {
-  if ($('.medbook-dropdown').css('display') == 'none') {
-    $('.medbook-dropdown').css('display', 'flex');
-  }
-  else if ($('.medbook-dropdown').css('display') == 'flex') {
-    $('.medbook-dropdown').css('display', 'none');
-  }
-});
-
 $('#menu-burger').click(function() {
-  $('#dropdown-burger').slideToggle(500);
+  $('#dropdown-burger').slideToggle(1000);
 });
