a4b59ae8832ff71faba32dbe3e248f722b755d8f
valeriadmitrievna1506 <valeriadmitrievna1506@gmail.com>
2020-10-09 12:47:16 +0300

    new header + edit analysis groups

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $('#menu-burger').click(function() {
   $('#dropdown-burger').slideToggle(1000);
 });
+
+$('.menu-burger-point').click(function () {
+  $(event.target.childNodes[1]).slideToggle(600);
+});
